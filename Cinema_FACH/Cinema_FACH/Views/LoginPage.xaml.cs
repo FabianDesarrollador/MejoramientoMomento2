689310338e6cfadaae793b6961e50485c67b37d3
FabianDesarrollador <desarrolladorsistemasfabian@gmail.com>
2018-10-27 13:15:26 -0500

    Consumo de API Login sin paquete MIcrosoft.BCL

--- Cinema_FACH/Cinema_FACH/Views/LoginPage.xaml.cs
+++ Cinema_FACH/Cinema_FACH/Views/LoginPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,29 @@ namespace Cinema_FACH.Views
         public LoginPage()
         {
             InitializeComponent();
+            loginboton.Clicked += loginboton_Clicked;
+        }
+
+        private async void loginboton_Clicked(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(user.Text))
+            {
+                await DisplayAlert("Error", "Debe Ingresar un Usuario", "Aceptar");
+                user.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(password.Text))
+            {
+                await DisplayAlert("Error", "Debe Ingresar una Contraseña", "Aceptar");
+                user.Focus();
+                return;
+            }
+
+            loginActivity.IsRunning = true;
+            HttpClient cliente = new HttpClient();
+            cliente.BaseAddress = new Uri(" https://misapis.azurewebsites.net")
+            string url = string.Format("/api/Seguridad ");
         }
     }
 }
